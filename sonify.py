ec1888f9a3b80487171c8b9f787f08dd8d905433
Liam Toney <toney.liam@gmail.com>
2020-02-17 02:32:44 -0900

    Format with black

--- sonify.py
+++ sonify.py
@@ -31,9 +31,21 @@ REFERENCE_VELOCITY = 1  # [m/s]
 MS_PER_S = 1000  # [ms/s]
 
 
-def sonify(network, station, channel, starttime, endtime, location='*',
-           freqmin=None, freqmax=None, speed_up_factor=200, fps=1,
-           output_dir=None, spec_win_dur=5, db_lim=None):
+def sonify(
+    network,
+    station,
+    channel,
+    starttime,
+    endtime,
+    location='*',
+    freqmin=None,
+    freqmax=None,
+    speed_up_factor=200,
+    fps=1,
+    output_dir=None,
+    spec_win_dur=5,
+    db_lim=None,
+):
 
     # Use current working directory if none provided
     if not output_dir:
@@ -44,9 +56,15 @@ def sonify(network, station, channel, starttime, endtime, location='*',
     client = Client('IRIS')
 
     print('Retrieving data...')
-    st = client.get_waveforms(network, station, location, channel,
-                              starttime - pad, endtime + pad,
-                              attach_response=True)
+    st = client.get_waveforms(
+        network,
+        station,
+        location,
+        channel,
+        starttime - pad,
+        endtime + pad,
+        attach_response=True,
+    )
     print('Done')
 
     if st.count() != 1:
@@ -62,12 +80,14 @@ def sonify(network, station, channel, starttime, endtime, location='*',
         is_infrasound = False
     # We can't figure out what type of sensor this is...
     else:
-        raise ValueError(f'Channel {tr.stats.channel} is not an infrasound or '
-                         'seismic channel!')
+        raise ValueError(
+            f'Channel {tr.stats.channel} is not an infrasound or seismic channel!'
+        )
 
     if not freqmax:
-        freqmax = np.min([tr.stats.sampling_rate / 2,
-                          HIGHEST_AUDIBLE_FREQUENCY / speed_up_factor])
+        freqmax = np.min(
+            [tr.stats.sampling_rate / 2, HIGHEST_AUDIBLE_FREQUENCY / speed_up_factor]
+        )
     if not freqmin:
         freqmin = LOWEST_AUDIBLE_FREQUENCY / speed_up_factor
 
@@ -87,8 +107,9 @@ def sonify(network, station, channel, starttime, endtime, location='*',
     tr_audio.taper(TAPER)
     audio_filename = os.path.join(output_dir, 'sonify-tmp.wav')
     print('Saving audio file...')
-    tr_audio.write(audio_filename, format='WAV', width=4, rescale=True,
-                   framerate=AUDIO_SAMPLE_RATE)
+    tr_audio.write(
+        audio_filename, format='WAV', width=4, rescale=True, framerate=AUDIO_SAMPLE_RATE
+    )
     print('Done')
 
     # MAKE VIDEO FILE
@@ -105,46 +126,69 @@ def sonify(network, station, channel, starttime, endtime, location='*',
 
         return spec_line, wf_line, time_box
 
-    fig, *fargs = _spectrogram(tr, starttime, endtime, is_infrasound,
-                               win_dur=spec_win_dur, db_lim=db_lim,
-                               freq_lim=(freqmin, freqmax))
+    fig, *fargs = _spectrogram(
+        tr,
+        starttime,
+        endtime,
+        is_infrasound,
+        win_dur=spec_win_dur,
+        db_lim=db_lim,
+        freq_lim=(freqmin, freqmax),
+    )
 
     # Create animation
     interval = ((1 / timing_tr.stats.sampling_rate) * MS_PER_S) / speed_up_factor
-    animation = FuncAnimation(fig, func=_march_forward, frames=times.size,
-                              fargs=fargs, interval=interval, blit=True)
+    animation = FuncAnimation(
+        fig,
+        func=_march_forward,
+        frames=times.size,
+        fargs=fargs,
+        interval=interval,
+        blit=True,
+    )
 
     video_filename = os.path.join(output_dir, 'sonify-tmp.mp4')
     print('Saving animation. This may take a while...')
-    animation.save(video_filename, dpi=DPI,
-                   progress_callback=lambda i, n: print('{:.1f}%'.format(((i + 1) / n) * 100), end='\r'))
+    animation.save(
+        video_filename,
+        dpi=DPI,
+        progress_callback=lambda i, n: print(
+            '{:.1f}%'.format(((i + 1) / n) * 100), end='\r'
+        ),
+    )
     print('\nDone')
 
     # MAKE COMBINED FILE
 
-    basename = '_'.join([tr.stats.network, tr.stats.station, tr.stats.channel,
-                         str(speed_up_factor) + 'x'])
+    basename = '_'.join(
+        [
+            tr.stats.network,
+            tr.stats.station,
+            tr.stats.channel,
+            str(speed_up_factor) + 'x',
+        ]
+    )
     output_filename = os.path.join(output_dir, f'{basename}.mp4')
     _ffmpeg_combine(audio_filename, video_filename, output_filename)
 
 
-def _spectrogram(tr, starttime, endtime, is_infrasound, win_dur=5, db_lim=None,
-                 freq_lim=None):
+def _spectrogram(
+    tr, starttime, endtime, is_infrasound, win_dur=5, db_lim=None, freq_lim=None
+):
     """
-    Make a combination trace and spectrogram plot for an infrasound or seismic
-    signal.
+    Make a combination trace and spectrogram plot for an infrasound or seismic signal.
 
     Args:
         tr: ObsPy Trace object (this code expects the response to be removed!)
         starttime: UTCDateTime
         endtime: UTCDateTime
         is_infrasound (bool): True if infrasound, False if seismic
-        win_dur: Segment length in seconds. This usually must be adjusted
-            depending upon the total duration of the signal (default: 5)
-        db_lim: Tuple defining min and max dB colormap cutoffs (default: None,
-            i.e. don't clip at all)
-        freq_lim: Tuple defining frequency limits for spectrogram plot (default:
-            None, i.e. use automatically scaled limits)
+        win_dur: Segment length in seconds. This usually must be adjusted depending upon
+            the total duration of the signal (default: 5)
+        db_lim: Tuple defining min and max dB colormap cutoffs (default: None, i.e.
+            don't clip at all)
+        freq_lim: Tuple defining frequency limits for spectrogram plot (default: None,
+            i.e. use automatically scaled limits)
 
     Returns:
         Figure with combination plot
@@ -162,29 +206,30 @@ def _spectrogram(tr, starttime, endtime, is_infrasound, win_dur=5, db_lim=None,
         rescale = 1e6  # Converting to μm/s
 
     fs = tr.stats.sampling_rate
-    nperseg = int(win_dur*fs)  # Samples
-    nfft = np.power(2, int(np.ceil(np.log2(nperseg)))+1)  # Pad fft with zeroes
+    nperseg = int(win_dur * fs)  # Samples
+    nfft = np.power(2, int(np.ceil(np.log2(nperseg))) + 1)  # Pad fft with zeroes
 
-    f, t, sxx = spectrogram(tr.data, fs, window='hann', nperseg=nperseg,
-                            nfft=nfft)
+    f, t, sxx = spectrogram(tr.data, fs, window='hann', nperseg=nperseg, nfft=nfft)
 
-    sxx_db = 20*np.log10(np.sqrt(sxx)/ref_val)  # [dB / Hz]
+    sxx_db = 20 * np.log10(np.sqrt(sxx) / ref_val)  # [dB / Hz]
 
     t_mpl = tr.stats.starttime.matplotlib_date + (t / mdates.SEC_PER_DAY)
 
-    fig, axes = plt.subplots(nrows=2, ncols=1, sharex='all',
-                             figsize=np.array(RESOLUTION) / DPI,
-                             gridspec_kw=dict(height_ratios=[2, 1],
-                                              hspace=0))
+    fig, axes = plt.subplots(
+        nrows=2,
+        ncols=1,
+        sharex='all',
+        figsize=np.array(RESOLUTION) / DPI,
+        gridspec_kw=dict(height_ratios=[2, 1], hspace=0),
+    )
 
-    axes[1].plot(tr.times('matplotlib'), tr.data*rescale, 'k', linewidth=0.5)
+    axes[1].plot(tr.times('matplotlib'), tr.data * rescale, 'k', linewidth=0.5)
     axes[1].set_ylabel(ylab)
     axes[1].grid(linestyle=':')
     max_value = np.abs(tr.copy().trim(starttime, endtime).data).max() * rescale
     axes[1].set_ylim(-max_value, max_value)
 
-    im = axes[0].pcolormesh(t_mpl, f, sxx_db, cmap=cc.m_rainbow,
-                            rasterized=True)
+    im = axes[0].pcolormesh(t_mpl, f, sxx_db, cmap=cc.m_rainbow, rasterized=True)
 
     axes[0].set_ylabel('Frequency (Hz)')
     axes[0].grid(linestyle=':')
@@ -195,16 +240,21 @@ def _spectrogram(tr, starttime, endtime, is_infrasound, win_dur=5, db_lim=None,
     axes[1].set_xlim(starttime.matplotlib_date, endtime.matplotlib_date)
     axes[1].xaxis_date()
     formatter = axes[1].xaxis.get_major_formatter()
-    formatter.scaled[1/mdates.MINUTES_PER_DAY] = '%H:%M'
-    formatter.scaled[1/mdates.SEC_PER_DAY] = '%H:%M:%S'
+    formatter.scaled[1 / mdates.MINUTES_PER_DAY] = '%H:%M'
+    formatter.scaled[1 / mdates.SEC_PER_DAY] = '%H:%M:%S'
     fig.autofmt_xdate()
 
     # Initialize animated stuff
     line_kwargs = dict(x=starttime.matplotlib_date, color='red', linewidth=1)
     spec_line = axes[0].axvline(**line_kwargs)
     wf_line = axes[1].axvline(**line_kwargs)
-    time_box = AnchoredText(s=starttime.strftime('%H:%M:%S'), pad=0.2,
-                            loc='lower right', borderpad=0, prop=dict(color='red'))
+    time_box = AnchoredText(
+        s=starttime.strftime('%H:%M:%S'),
+        pad=0.2,
+        loc='lower right',
+        borderpad=0,
+        prop=dict(color='red'),
+    )
     axes[0].add_artist(time_box)
 
     im.set_clim(db_lim)
@@ -213,21 +263,24 @@ def _spectrogram(tr, starttime, endtime, is_infrasound, win_dur=5, db_lim=None,
     fig.subplots_adjust(right=0.85)
 
     box = axes[0].get_position()
-    cax = fig.add_axes([box.xmax+box.height/20, box.y0, box.height/20,
-                        box.height])
+    cax = fig.add_axes(
+        [box.xmax + box.height / 20, box.y0, box.height / 20, box.height]
+    )
     cbar = fig.colorbar(im, cax)
     cbar.set_label(clab)
 
-    axes[0].set_title('.'.join([tr.stats.network, tr.stats.station,
-                                tr.stats.location, tr.stats.channel]))
+    axes[0].set_title(
+        '.'.join(
+            [tr.stats.network, tr.stats.station, tr.stats.location, tr.stats.channel]
+        )
+    )
 
     return fig, spec_line, wf_line, time_box
 
 
 def _ffmpeg_combine(audio_filename, video_filename, output_filename):
     """
-    Combine video and audio files into a single movie. Uses a system call to
-    ffmpeg.
+    Combine video and audio files into a single movie. Uses a system call to ffmpeg.
 
     Args:
         audio_filename (str): Audio file to use (full path)
@@ -235,9 +288,23 @@ def _ffmpeg_combine(audio_filename, video_filename, output_filename):
         output_filename (str): Output filename (full path)
     """
 
-    args = ['ffmpeg', '-y', '-v', 'warning', '-i', video_filename, '-i',
-            audio_filename, '-c:v', 'copy', '-c:a', 'aac', '-ac', '2',
-            output_filename]
+    args = [
+        'ffmpeg',
+        '-y',
+        '-v',
+        'warning',
+        '-i',
+        video_filename,
+        '-i',
+        audio_filename,
+        '-c:v',
+        'copy',
+        '-c:a',
+        'aac',
+        '-ac',
+        '2',
+        output_filename,
+    ]
     print('Combining video and audio using ffmpeg...')
     code = subprocess.call(args)
     if code == 0:
@@ -245,5 +312,6 @@ def _ffmpeg_combine(audio_filename, video_filename, output_filename):
         os.remove(audio_filename)
         os.remove(video_filename)
     else:
-        raise OSError('Issue with ffmpeg conversion. Check error messages and '
-                      'try again.')
+        raise OSError(
+            'Issue with ffmpeg conversion. Check error messages and try again.'
+        )
