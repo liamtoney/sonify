773bb997e50613e3347e7fdf98c8786c590fa89f
Liam Toney <toney.liam@gmail.com>
2021-10-31 00:20:20 -0800

    Move comment position

--- sonify/sonify.py
+++ sonify/sonify.py
@@ -311,9 +311,8 @@ def _spectrogram(
         tr.data, fs, window='hann', nperseg=nperseg, noverlap=nperseg // 2, nfft=nfft
     )
 
-    sxx_db = 10 * np.log10(
-        sxx / (ref_val ** 2)
-    )  # [dB rel. (ref_val <ref_val_unit>)^2 Hz^-1]
+    # [dB rel. (ref_val <ref_val_unit>)^2 Hz^-1]
+    sxx_db = 10 * np.log10(sxx / (ref_val ** 2))
 
     t_mpl = tr.stats.starttime.matplotlib_date + (t / mdates.SEC_PER_DAY)
 
