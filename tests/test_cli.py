bc42255142b6ff06ac29005f481e77cbe01f7933
Liam Toney <toney.liam@gmail.com>
2021-11-18 00:42:23 -0900

    Add some CLI tests

---
+++ tests/test_cli.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+import pytest
+
+
+def test_cli_help():
+    subprocess.run(['sonify', '--help'], check=True, stdout=subprocess.DEVNULL)
+
+
+@pytest.mark.skipif(
+    not os.environ.get('GITHUB_SHA'), reason='must be run on GitHub Actions'
+)
+def test_cli_version():
+    output = subprocess.run(
+        ['sonify', '--version'], capture_output=True, text=True
+    ).stdout.strip()
+    assert output == 'sonify, rev. {}'.format(os.environ['GITHUB_SHA'][:7])
