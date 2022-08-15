c65607cf8102890ab931cb36a938034ab25319d0
Liam Toney <38269494+liamtoney@users.noreply.github.com>
2022-08-15 13:41:02 -0800

    More specific directory existence check

--- sonify/sonify.py
+++ sonify/sonify.py
@@ -119,7 +119,7 @@ def sonify(
     if not output_dir:
         output_dir = Path().cwd()
     output_dir = Path(str(output_dir)).expanduser().resolve()
-    if not output_dir.exists():
+    if not output_dir.is_dir():
         raise FileNotFoundError(f'Directory {output_dir} does not exist!')
 
     client = Client('IRIS')
