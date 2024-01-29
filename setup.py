d6ed777796ac8ea2ae26e5f64a6acd63693d0850
Web Source <web.system.management@gmail.com>
2024-01-29 13:37:35 +0200

    Update setup.py
    Resolves setuptools.extern.packaging.version.InvalidVersion: Invalid version.

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
     name='sonify',
     version=__version__,
     packages=find_packages(),
+    setuptools_git_versioning={
+        "enabled": True,
+    },
     entry_points=dict(console_scripts='sonify = sonify.sonify:main'),
+    setup_requires=["setuptools-git-versioning<2"]            
+
 )
