4101e313cb34da1e85114c7e5ec233b9c1730bc0
Liam Toney <ldtoney@alaska.edu>
2024-01-30 11:08:16 -0700

    Update version number to be PEP 440 compliant (#18)
    * Make version # PEP 440 compliant
    
    * Fix version CLI test

--- sonify/__init__.py
+++ sonify/__init__.py
@@ -1,18 +1,21 @@
 import subprocess
 from pathlib import Path
 
-__version__ = subprocess.run(
-    [
-        'git',
-        '-C',
-        Path(__file__).resolve().parent,
-        'rev-parse',
-        '--short=7',
-        'HEAD',
-    ],
-    capture_output=True,
-    text=True,
-).stdout.strip()
+__version__ = (
+    '0+g'  # Makes the version number PEP 440 compliant
+    + subprocess.run(
+        [
+            'git',
+            '-C',
+            Path(__file__).resolve().parent,
+            'rev-parse',
+            '--short=7',  # First 7 characters of the commit hash
+            'HEAD',
+        ],
+        capture_output=True,
+        text=True,
+    ).stdout.strip()
+)
 
 del subprocess
 del Path
