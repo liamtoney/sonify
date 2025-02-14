943d45c7ed19f4f383a9d583e847b92ceaebeef2
Liam Toney <liam@liam.earth>
2025-02-14 01:45:59 -0700

    Use setuptools_scm

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    use_scm_version=dict(
+        local_scheme=lambda version: '+' + version.node,
+        version_scheme=lambda version: '0',
+        version_file='sonify/_version.py',
+    )
+)
--- sonify/__init__.py
+++ sonify/__init__.py
@@ -1,23 +1,2 @@
-import subprocess
-from pathlib import Path
-
-__version__ = (
-    '0+g'  # Makes the version number PEP 440 compliant
-    + subprocess.run(
-        [
-            'git',
-            '-C',
-            Path(__file__).resolve().parent,
-            'rev-parse',
-            '--short=7',  # First 7 characters of the commit hash
-            'HEAD',
-        ],
-        capture_output=True,
-        text=True,
-    ).stdout.strip()
-)
-
-del subprocess
-del Path
-
+from ._version import __version__
 from .sonify import sonify
