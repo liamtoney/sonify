fc09ce929b5ac9ea5c597ecffb423b37f6aefa9b
Liam Toney <liam@liam.earth>
2025-08-22 23:49:25 -0600

    Ensure commit hash in version is right length

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     use_scm_version=dict(
-        local_scheme=lambda version: '+' + version.node,
+        local_scheme=lambda version: '+' + version.node[:8],  # "g<7_char_commit_hash>"
         version_scheme=lambda version: '0',
     )
 )
